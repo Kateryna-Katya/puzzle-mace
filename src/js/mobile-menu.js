b957376ff6c48cc6fc8e514ea13a96c67f68d270
Kateryna <katyaukr2014@gmail.com>
2025-07-07 11:10:49 +0300

    add

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.querySelector('.menu-button');
+    const iconContainer = menuBtn.querySelector('.icon');
+    const modalMenu = document.querySelector('.modal');
+    const menuItem = document.querySelectorAll('.menu-list-item');
+    const header = document.querySelector('.header');
+  
+    let isMenuOpen = false;
+  
+    const burgerIcon = `
+<img src="/img/header/burger.png" alt="Burger" loading="lazy" width="48" height="48">
+    `;
+  
+    const crossIcon = `
+<img src="/img/header/burger.png" alt="Burger" loading="lazy" width="48" height="48">
+    `;
+  
+    menuBtn.addEventListener('click', () => {
+      isMenuOpen = !isMenuOpen;
+      iconContainer.innerHTML = isMenuOpen ? crossIcon : burgerIcon;
+      isMenuOpen ? openModal() : closeModal();
+    });
+  
+    menuItem.forEach(item =>
+      item.addEventListener('click', () => {
+        closeModal();
+        iconContainer.innerHTML = burgerIcon;
+        isMenuOpen = false;
+      })
+    );
+  
+    window.addEventListener('scroll', blurHeader);
+  
+    function openModal() {
+      modalMenu.style.display = 'block';
+    }
+  
+    function closeModal() {
+      modalMenu.style.display = 'none';
+    }
+  
+    function blurHeader() {
+      if (!header) return;
+      if (window.scrollY > 60) {
+        header.classList.add('header-scroll');
+      } else {
+        header.classList.remove('header-scroll');
+      }
+    }
+  });
